50550b6f94f7d7c8d34aad5a1ac6148db9681621
Amaya Cranford <cranfoas@dukes.jmu.edu>
2026-04-08 08:06:31 -0400

    forgot to save

--- ref/js/part3.js
+++ ref/js/part3.js
@@ -5,8 +5,8 @@ document.getElementById('api-1-btn').addEventListener('click', async () => {
     // Make a request to your first API here. Put the response's data in output-1.
     // If your response has no body, put the status code in output-1.
 
-    const response = await fetch("https://dogapi.dog/api/v2/breeds/9d7c4db8-b9cf-4ed3-af8e-86fc56fbf251");
-    console.log(response);
+    const response = await mockFetch("https://www.themealdb.com/api/json/v1/1/search.php?f=b");
+    console.log("Meal DB response:", response);
     const data = await response.json();
     output1.textContent = JSON.stringify(data, null, 2);
 });
@@ -14,7 +14,7 @@ document.getElementById('api-1-btn').addEventListener('click', async () => {
 document.getElementById('api-2-btn').addEventListener('click', async () => {
     // Make a request to your second API here. Put the response's data in output-2.
     // If your response has no body, put the status code in output-2.
-    
+
     const response = await fetch("https://corsproxy.io/?" + encodeURIComponent("https://xkcd.com/303/info.0.json"));
     console.log(response);
     const data = await response.json();
